d63e87b1c8c8361107a5fe6a0466ec1e3d68625c
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2017-07-03 22:50:34 +0900

    Support Carthage

---
+++ ZipArchive/ZipArchive.h
@@ -0,0 +1,19 @@
+//
+//  ZipArchive.h
+//  ZipArchive
+//
+//  Created by Yasuhiro Hatta on 2017/07/03.
+//  Copyright © 2017年 yaslab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZipArchive.
+FOUNDATION_EXPORT double ZipArchiveVersionNumber;
+
+//! Project version string for ZipArchive.
+FOUNDATION_EXPORT const unsigned char ZipArchiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZipArchive/PublicHeader.h>
+
+
