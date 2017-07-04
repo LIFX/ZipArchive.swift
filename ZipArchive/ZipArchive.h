c349cb23f20851a6caef509949353734d799e453
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2017-07-04 20:21:49 +0900

    Add unit test to Xcode project

--- ZipArchive/ZipArchive.h
+++ ZipArchive/ZipArchive.h
@@ -3,10 +3,10 @@
 //  ZipArchive
 //
 //  Created by Yasuhiro Hatta on 2017/07/03.
-//  Copyright © 2017年 yaslab. All rights reserved.
+//  Copyright © 2017 yaslab. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for ZipArchive.
 FOUNDATION_EXPORT double ZipArchiveVersionNumber;
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double ZipArchiveVersionNumber;
 FOUNDATION_EXPORT const unsigned char ZipArchiveVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ZipArchive/PublicHeader.h>
-
-
